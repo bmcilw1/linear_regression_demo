fccc5c23ca5732874e2d462db1e43f9431c77ae5
Brian McIlwain <brianwmcilwain@gmail.com>
2018-04-02 21:56:23 -0700

    formatting

--- im_challenged.py
+++ im_challenged.py
@@ -6,12 +6,24 @@ import matplotlib.pyplot as plt
 dataframe = pd.read_csv('challenge_dataset.txt')
 x_values = dataframe[[0]]
 y_values = dataframe[[1]]
+test_val = (dataframe[[0]].iloc[0], dataframe[[1]].iloc[0])
+
+#print(test_val)
+
+print("Fitting...")
 
 #train model on data
 body_reg = linear_model.LinearRegression()
 body_reg.fit(x_values, y_values)
 
+print("Done!")
+
+print("Sample: %i, %f" % (test_val, body_reg.predict(test_val)))
+
 #visualize results
+'''
 plt.scatter(x_values, y_values)
 plt.plot(x_values, body_reg.predict(x_values))
+plt.scatter(test_val, body_reg.predict(test_val))
 plt.show()
+'''
